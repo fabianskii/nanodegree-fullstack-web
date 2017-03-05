a4c8dbff78be1e74bbcfae55693f5d8dcc2616f0
Fabian Isele <Fabian Isele>
2017-03-05 03:22:26 +0100

    started with tournament project. current status: need report match for debugging of playerStandings.

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -13,26 +13,44 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
-
+    DB = connect()
+    connection = DB.cursor()
+    connection.execute("DELETE FROM match")
+    DB.commit()
+    DB.close()
 
 def deletePlayers():
     """Remove all the player records from the database."""
-
+    DB = connect()
+    connection = DB.cursor()
+    connection.execute("DELETE FROM player")
+    DB.commit()
+    DB.close()
 
 def countPlayers():
     """Returns the number of players currently registered."""
-
+    DB = connect()
+    connection = DB.cursor()
+    connection.execute("SELECT count(name) as playerCount FROM player")
+    response = {'player_count': connection.fetchone()[0]}
+    DB.commit()
+    DB.close()
+    return response['player_count']
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
   
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
-
+    DB = connect()
+    c = DB.cursor()
+    c.execute("INSERT INTO player (name) VALUES (%s)", (name,))
+    DB.commit()
+    DB.close()
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
@@ -40,15 +58,23 @@ def playerStandings():
     The first entry in the list should be the player in first place, or a player
     tied for first place if there is currently a tie.
 
-    Returns:
+   Returns:
       A list of tuples, each of which contains (id, name, wins, matches):
         id: the player's unique id (assigned by the database)
         name: the player's full name (as registered)
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
-
-
+    DB = connect()
+    connection = DB.cursor()
+    connection.execute(" SELECT r.*, count(m.match_id) FROM ranking as r, match as m "
+                       "WHERE m.player_1_id = r.player_id OR m.player_2_id = r.player_id "
+                       "GROUP BY r.player_id,r.name,r.wins;")
+    fetchall = connection.fetchall()
+    print "**** %s" % fetchall
+    response = ({'id': str(row[0]),'name': str(row[1]), 'wins': str(row[2]), 'matches':str(row[3])} for row in fetchall)
+    print "#### %s" % response
+    return response;
 def reportMatch(winner, loser):
     """Records the outcome of a single match between two players.
 
@@ -56,7 +82,7 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
+    
  
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,17 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+CREATE TABLE player ( player_id SERIAL PRIMARY KEY,
+                      name TEXT
+                     );
 
+CREATE TABLE match ( match_id SERIAL PRIMARY KEY,
+                     player_1_id INTEGER REFERENCES player (player_id),
+                     player_2_id INTEGER REFERENCES player (player_id),
+                     player_winner INTEGER REFERENCES player (player_id)
+                     );
+
+CREATE VIEW ranking as
+SELECT p.player_id, p.name, count(m.match_id) as wins
+FROM player as p, match as m WHERE p.player_id = m.player_winner
+GROUP BY p.name, p.player_id ORDER BY wins;
